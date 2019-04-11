0a6a87ddcd5fa0fd1a01373fde4f88109d43f252
azrap <azraap@gmail.com>
2019-04-10 23:03:00 -0700

    fixing the code for ask #2 + adding another article component to the html.

--- Article/Article.js
+++ Article/Article.js
@@ -7,14 +7,15 @@ class Article {
     // create a reference to the ".expandButton" class. 
     this.expandButton=this.domArticle.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.innerHTML='expand';
+    this.expandButton.innerText='expand';
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    this.expandButton.addEventListener('click', this.ex;
+    this.expandButton.addEventListener('click', () => {this.expandArticle()});
   }
+  // WATCH THE WTF VIDEO!
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    this.domArticle.classList.toggle('article-open')
+    this.domArticle.classList.toggle('article-open');
 
   }
 }
@@ -27,7 +28,14 @@ class Article {
 
 */
 
-let articles=document.querySelectorAll('.article');
-articles.forEach(function(article){
-  return new Article(article);
-})
+// let articles=document.querySelectorAll('.article');
+
+// articles.forEach( (article) => {
+//   return new Article(article);
+// })
+
+let articles = document.querySelectorAll('.article').forEach( article => {
+  new Article(article);
+});
+
+
